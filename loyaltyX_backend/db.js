fd7c534f66f35bf1130a2fcf949d1eb35f42631b
khaled-muhammad <khaledmuhmmed99@gmail.com>
2025-11-17 09:33:32 +0200

    update backend to work on linux and macos correctly + Cors updates for safety.

--- loyaltyX_backend/db.js
+++ loyaltyX_backend/db.js
@@ -4,13 +4,13 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const sequelize = new Sequelize(
-  process.env.DATABASE,
-  process.env.USER,
-  process.env.PASSWORD,
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
-    host: process.env.HOST,
+    host: process.env.DB_HOST,
     dialect: 'mysql',
-    port: process.env.DBPORT || 3306,
+    port: process.env.DB_PORT || 3306,
   }
 )
 
--- loyaltyX_backend/index.js
+++ loyaltyX_backend/index.js
@@ -14,7 +14,12 @@ const port = process.env.PORT
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}))
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
