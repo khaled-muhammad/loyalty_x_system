41ea4224465ab66e227903e50ab214bc6a6ea53d
khaled-muhammad <khaledmuhmmed99@gmail.com>
2025-11-18 18:48:16 +0200

    test

--- loyalty/lib/consts.dart
+++ loyalty/lib/consts.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
 
 // Plz change base url before running the flutter app to avoid any issues and make it match your device ip
-final dio = Dio(BaseOptions(baseUrl: 'http://192.168.1.30:43858/api/'));
+final dio = Dio(BaseOptions(baseUrl: 'https://loyalty-x-system.onrender.com/api/'));
+// final dio = Dio(BaseOptions(baseUrl: 'http://192.168.1.30:43858/api/'));
